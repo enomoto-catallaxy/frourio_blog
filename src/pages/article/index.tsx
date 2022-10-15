5febb0cfba97538519f57269b457f091cda8c57a
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-10-15 17:15:16 +0900

    (article) apply styled

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -1,4 +1,3 @@
-import { useCallback, useState } from 'react'
 import useAspidaSWR from '@aspida/swr'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
@@ -7,6 +6,7 @@ import Link from 'next/link'
 import Layout from '~/components/Layout'
 import { pagesPath } from '~/utils/$path'
 import { apiClient } from '~/utils/apiClient'
+import styled from 'styled-components'
 
 export type OptionalQuery = {
   search: string
@@ -20,45 +20,49 @@ const ArticleList: NextPage = () => {
     query: { search }
   })
   return (
-    <Layout>
-      <Head>
-        <title>Articles</title>
-      </Head>
-      <h1>Articles</h1>
-      {articleList ? (
-        articleList.length ? (
-          <>
-            {search && (
+    <Wrapper>
+      <Layout>
+        <Head>
+          <title>Articles</title>
+        </Head>
+        <h1>Articlesではない</h1>
+        {articleList ? (
+          articleList.length ? (
+            <>
+              {search && (
+                <span>
+                  Results for <code>{search}</code>.
+                </span>
+              )}
+              <ul>
+                {articleList.map((article) => (
+                  <li key={article.id}>
+                    <Link
+                      href={pagesPath.article.entry.$url({
+                        query: { id: article.id }
+                      })}
+                    >
+                      {article.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : (
+            search && (
               <span>
-                Results for <code>{search}</code>.
+                Not found for <code>{search}</code>.
               </span>
-            )}
-            <ul>
-              {articleList.map((article) => (
-                <li key={article.id}>
-                  <Link
-                    href={pagesPath.article.entry.$url({
-                      query: { id: article.id }
-                    })}
-                  >
-                    {article.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </>
-        ) : (
-          search && (
-            <span>
-              Not found for <code>{search}</code>.
-            </span>
+            )
           )
-        )
-      ) : (
-        <span>Loading...</span>
-      )}
-    </Layout>
+        ) : (
+          <span>Loading...</span>
+        )}
+      </Layout>
+    </Wrapper>
   )
 }
 
 export default ArticleList
+
+const Wrapper = styled.div``
