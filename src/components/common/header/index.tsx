f37a0b8f109f74f1b52e7ce80e333ea970ad99c1
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-10-17 03:47:31 +0900

    (Header)add link to root

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,12 +1,19 @@
 import { memo } from 'react'
 import { Loader } from '@mantine/core'
 import styled from 'styled-components'
+import { useRouter } from 'next/router'
+import { pagesPath } from '~/utils/$path'
 
 export const Header = memo(() => {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push(pagesPath.$url())
+  }
   return (
     <Wrapper>
       <Loader size={100} />
-      header
+      <ToRoot onClick={handleClick}>ROOT</ToRoot>
     </Wrapper>
   )
 })
@@ -22,4 +29,10 @@ const Wrapper = styled.div`
   align-items: center;
 
   background-color: lightgreen;
+`
+const ToRoot = styled.h1`
+  :hover {
+    color: blue;
+    cursor: pointer;
+  }
 `
