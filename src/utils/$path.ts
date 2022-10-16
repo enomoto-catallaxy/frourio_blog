a9578b7a89fd0d57016fdc8ffc4ebc147a375378
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-10-17 00:03:36 +0900

    (home)create myhome page

--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -8,6 +8,9 @@ export const pagesPath = {
     },
     $url: (url?: { query?: OptionalQuery0, hash?: string }) => ({ pathname: '/article' as const, query: url?.query, hash: url?.hash })
   },
+  "home": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/home' as const, hash: url?.hash })
+  },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
